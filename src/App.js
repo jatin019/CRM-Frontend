d940502ad1cb2e653649ed5978d580b458090825
Jatin Singh <js.jatinsingh19@gmail.com>
2024-06-20 17:17:51 +0530

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import React from 'react';
 import { DefaultLayout } from "./layout/DefaultLayout"
 import { Entry } from "./pages/entry/Entry.page";
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 function App() {
   return (
     <div className="App">
     {/* <Entry /> */}
-    <DefaultLayout>//Dashboard</DefaultLayout>
+    <DefaultLayout>
+      <Dashboard />
+    </DefaultLayout>
     </div>
   );
 }
