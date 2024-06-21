21639edf04d22d26faf6ef41eb3e12723c267031
Jatin Singh <js.jatinsingh19@gmail.com>
2024-06-21 23:59:46 +0530

    ticket landing page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { DefaultLayout } from "./layout/DefaultLayout"
 //import { Dashboard } from './pages/dashboard/Dashboard.page';
 import { AddTicket } from './pages/new-ticket/AddTicket.page';
 import { TicketLists } from './pages/ticket-listing/TicketLists.page';
+import { Ticket } from './pages/ticket/Ticket.page';
 function App() {
   return (
     <div className="App">
@@ -12,7 +13,8 @@ function App() {
     <DefaultLayout>
       {/*<Dashboard /> */}
       {/*<AddTicket/> */}
-      <TicketLists/>
+      {/*<TicketLists/> */}
+      <Ticket/>
     </DefaultLayout>
     </div>
   );
