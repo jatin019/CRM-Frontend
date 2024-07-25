c292ec005c1eeaa4a17972e85b1bd388fabe7e9c
Jatin Singh <js.jatinsingh19@gmail.com>
2024-07-25 21:38:50 +0530

    fetch all tickets api integrated

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -6,7 +6,7 @@ return new Promise(async (resolve, reject) => {
    const result = await axios.get(
         'http://localhost:3001/v1/ticket',
         {headers: 
-            { Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhdmlAZ21haWwuY29tIiwiaWF0IjoxNzIxOTIwMDA4LCJleHAiOjE3MjE5MjA5MDh9.EEmJIX1AKxgUCjpIPzKv1rVOPYBrzSU7dNh7lyUwg6w",
+            { Authorization: sessionStorage.getItem("accessJWT"),
             }}        
             )
             resolve(result)
